b6e462c570773997a9957cb3c035b56168e44517
Lucas Araújo <lucasaraujodesa@hotmail.com>
2023-10-19 12:08:57 -0300

    feat: prisma adapter

---
+++ src/@types/next-auth.d.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import nextAuth from 'next-auth'
+
+declare module 'next-auth' {
+  interface User {
+    id: string
+    name: string
+    email: string
+    username: string
+    avatar_url: string
+  }
+}
