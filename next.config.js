ebfbcf1e4a431370356571b78f33df9acb252853
Lucas Araújo <lucasaraujodesa@hotmail.com>
2023-11-03 22:06:51 -0300

    feat: update-profile page and api route

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: [
-    'page.tsx',
-    'api.ts',
-    'api.tsx'
-  ]
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  images: {
+    domains: ['https://lh3.googleusercontent.com/'],
+  },
 }
 
 module.exports = nextConfig
--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -22,6 +22,7 @@ import { getWeekDays } from '@/utils/get-week-days'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { convertTimeStringToMinutes } from '@/utils/convert-time-to-minutes'
 import { api } from '@/lib/axios'
+import { useRouter } from 'next/router'
 
 const timeIntervalsFormSchema = z.object({
   intervals: z
@@ -86,6 +87,8 @@ export default function TimeIntervals() {
     },
   })
 
+  const router = useRouter()
+
   const weekDays = getWeekDays()
 
   const { fields } = useFieldArray({
@@ -101,6 +104,8 @@ export default function TimeIntervals() {
     await api.post('/users/time-intervals', {
       intervals,
     })
+
+    await router.push('/register/update-profile')
   }
 
   return (
