e541b840ccd12390cf98282d9f8c6d0796fc2235
Lucas Araújo <lucasaraujodesa@hotmail.com>
2023-11-24 22:31:50 -0300

    chore: google api config

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -18,6 +18,9 @@ export function buildNextAuthOptions(
           params: {
             scope:
               'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
           },
         },
 
